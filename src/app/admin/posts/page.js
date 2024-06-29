a202a64416f63c8807f3d3c79629bd2978707b0f
Elton Sidney Pedro de Sousa <sidneymelodia@Eltons-MBP.home>
2024-06-29 09:28:32 +0200

    created initial structure for admin posts page

--- src/app/admin/posts/page.js
+++ src/app/admin/posts/page.js
@@ -1,6 +1,31 @@
+import Link from "next/link";
+
 export default function Page() {
 
-    return <>
-        <h1>Posts page</h1>
-    </>
+    return (
+        <>
+            <div className="flex justify-between mb-2 mt-5">
+                <h1 className="text-[30px] font-semibold">Posts</h1>
+                <Link href="/admin/posts/create" className="text-white bg-indigo-950 px-4 py-2 rounded-lg">Create Post</Link>
+            </div>
+            <hr />
+
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Title</td>
+                        <td>Body</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
 }
